e98566ca428e4cbc03260d926abecf2845134fb1
Makukas <marijukas@gmail.com>
2017-11-18 13:12:52 +0200

    fixed several bugs

--- functions.js
+++ functions.js
@@ -8,7 +8,7 @@ function generatePassword(){
   var capitalLetters1 = document.getElementById("capitalLetters").checked;
   var specialChar1 = document.getElementById("specialChar").checked;
 
-  if(numbers1 && smallLetters1 && capitalLetters1 && specialChar1 == false){
+  if((numbers1 && smallLetters1 && capitalLetters1 && specialChar1) == false){
     window.alert("You didn't select any options!")
     return;
   }
@@ -61,7 +61,7 @@ function generatePassword(){
       }
       infiniteLoopStop++;
       if(infiniteLoopStop == 100){
-        i = lenght;
+        i = lenght + lenght;
       }
   }
 
